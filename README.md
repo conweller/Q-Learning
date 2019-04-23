4a38e213518234d85c12d8abf63ec6d9ee79e881
Connor Onweller <onweller@udel.edu>
2019-04-23 11:53:21 -0400

    added to board class

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Q-Learning Algorithm
 This program uses a q-learning to solve to determine the best path to a goal
 state.
+
+Please run with python3 (version > 3.7)
--- board.py
+++ board.py
@@ -1,10 +1,51 @@
 """
-This module contains the Board class and methods
+This module contains the Board and methods
 """
 
-import copy as c
+from dataclasses import dataclass
+from enum import Enum
+
+
+class BlockTag(Enum):
+    """
+    A value specifying specifying whether the position is either forbidden, a
+    wall, or the goal
+    """
+    NORMAL = 0
+    WALL = 1
+    FORBIDDEN = 2
+    GOAL = 3
+
+@dataclass
+class Block:
+    """
+    A square space on the board
+    tag:    The BlockTag value for the state
+    q_val:  The q value at that state
+    """
+    tag: BlockTag
+    q_val: int
 
 
 class Board:
+    """The current state of the board, with q-values and agent position
+
+    Attributes:
+        agent_pos:  The agent's current position
+        blocks:     The set of positions on the board
+
+    """
+
     def __init__(self, data):
         self.data = list(map(int, data[0:3]))
+        self.blocks = []
+
+    def _populate_board(self, goal, forbbidden, wall):
+        for _ in range(0, 12):
+            self.blocks.append(Block(0, 0))
+
+    def x(self):
+        return 1
+
+    def y(self):
+        return 1
